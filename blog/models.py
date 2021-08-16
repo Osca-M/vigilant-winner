9761d17b5b7836d8719a73505f1732ad808dda31
Osca-M <oscamwongera@gmail.com>
2021-08-16 10:01:39 +0300

    Create blog model, configure time settigns

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 STATUS = (
@@ -8,8 +10,10 @@ STATUS = (
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    title = models.CharField(max_length=255, unique=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
